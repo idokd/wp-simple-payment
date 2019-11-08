2f03c47f118d7c9bdae4c72edd60ddb4898770d8
Ido Kobelkowsky <idokd@users.noreply.github.com>
2019-11-08 18:53:49 +0200

    1.5.5

--- vendor/yalla-ya/simple-payment/Engines/Cardcom.php
+++ vendor/yalla-ya/simple-payment/Engines/Cardcom.php
@@ -334,7 +334,7 @@ class Cardcom extends Engine {
 
     $post['RecurringPayments.FlexItem.IsPriceIncludeVat'] = 'true'; // Must be true - API requirement
     // TODO: assure to verifiy first_name / lasstname or use full name
-    $post['Account.CompanyName'] = isset($params['tax_id']) && $params['tax_id'] ? $params['tax_id'] : $params['first_name'].' '.$params['last_name'];
+    $post['Account.CompanyName'] = isset($params['tax_id']) && $params['tax_id'] ? $params['tax_id'] : (isset($params['full_name']) && $params['full_name'] ? $params['full_name'] : $params['first_name'].' '.$params['last_name']);
     // Not in use:
     //  Account.AccountId	, 
     //  Account.DontCheckForDuplicate	RecurringPayments.RecurringId
