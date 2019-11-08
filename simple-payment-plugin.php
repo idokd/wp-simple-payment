d82d89c087a7a7b45e13a5441fced63fc2b10d5c
Ido Kobelkowsky <idokd@users.noreply.github.com>
2019-11-08 19:26:41 +0200

    1.5.9

--- simple-payment-plugin.php
+++ simple-payment-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Simple Payment
  * Plugin URI: https://simple-payment.yalla-ya.com
  * Description: Simple Payment enables integration with multiple payment gateways, and customize multiple payment forms.
- * Version: 1.5.8
+ * Version: 1.5.9
  * Author: Ido Kobelkowsky / yalla ya!
  * Author URI: https://github.com/idokd
  * License: GPLv2
@@ -992,7 +992,7 @@ class SimplePaymentPlugin extends SimplePayment\SimplePayment {
     }
     $result = $wpdb->get_results( $sql , 'ARRAY_A' );
     $data = count($result) ? $result[0] : null;
-    if ($data && isset($data['parameters']) && $data['parameters']) $data = array_merge($data, $data['parameters']);
+    if ($data && isset($data['parameters']) && $data['parameters']) $data = array_merge($data, json_decode($data['parameters']));
     return($data);
   }
 
@@ -1036,20 +1036,20 @@ class SimplePaymentPlugin extends SimplePayment\SimplePayment {
         'simple-payment-gb-style-css', 
         plugin_dir_url( __FILE__ ).'/addons/gutenberg/blocks.style.build.css', // Block style CSS.
         array( 'wp-editor' ), 
-        null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: 1.5.8File modification time.
+        null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: 1.5.9File modification time.
       );
       wp_register_script(
         'simple-payment-gb-block-js',
         plugin_dir_url( __FILE__ ).'/addons/gutenberg/blocks.build.js',
         array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-shortcode', 'wp-editor' ), 
-        null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: 1.5.8filemtime — Gets file modification time.
+        null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: 1.5.9filemtime — Gets file modification time.
         true 
       );
       wp_register_style(
         'simple-payment-gb-editor-css', 
         plugin_dir_url( __FILE__ ).'/addons/gutenberg/blocks.editor.build.css', 
         array( 'wp-edit-blocks' ), 
-        null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: 1.5.8File modification time.
+        null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: 1.5.9File modification time.
       );
       wp_localize_script(
         'simple-payment-gb-block-js',
