2fe3d8d0ac95e83d35e0408a006aa3d6b3e6c6ea
Ido Kobelkowsky <ido@yalla-ya.com>
2024-10-14 13:13:37 -0700

    2.3.5

--- simple-payment-plugin.php
+++ simple-payment-plugin.php
@@ -3,13 +3,13 @@
  * Plugin Name: Simple Payment
  * Plugin URI: https://simple-payment.yalla-ya.com
  * Description: Simple Payment enables integration with multiple payment gateways, and customize multiple payment forms.
- * Version: 2.3.4
+ * Version: 2.3.5
  * Author: Ido Kobelkowsky / yalla ya!
  * Author URI: https://github.com/idokd
  * License: GPLv2
  * Text Domain: simple-payment
  * Domain Path: /languages
- * WC tested up to: 7.5.1
+ * WC tested up to: 9.3.3
  * WC requires at least: 2.6
  */
 
